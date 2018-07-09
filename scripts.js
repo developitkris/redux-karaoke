7f2f27772abe966da2c83011a43eb195f7819b07
Kristi <krsy3ii@yahoo.com>
2018-07-09 10:34:28 -0700

    add reducer, jest, redux store, render, and subscription to initial redux.  Each line of song lyrics should display on click

--- scripts.js
+++ scripts.js
@@ -7,16 +7,53 @@ const initialState = {
   arrayPosition: 0,
 }
 
-// REDUCER WILL GO HERE
-
+// REDUX REDUCER
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'NEXT_LYRIC':
+      let newArrayPosition = state.arrayPosition +1;
+      let newState = {
+        songLyricsArray: state.songLyricsArray,
+        arrayPosition: newArrayPosition,
+      }
+      return newState;
+    default:
+      return state;
+  }
+}
 // JEST TESTS + SETUP WILL GO HERE
+const { expect } = window;
+
+expect(reducer(initialState, { type: null })).toEqual(initialState);
+
+expect(reducer(initialState, { type: 'NEXT_LYRIC' })).toEqual({
+  songLyricsArray: songLyricsArray,
+  arrayPosition: 1
+});
 
 //REDUX STORE
-const {createStore} = Redux;
+const { createStore } = Redux;
 const store= createStore(reducer);
 console.log(store.getState());
 
+//RENDERING STATE IN DOM
+const renderLyrics = () => {
+  const lyricsDisplay = document.getElementById('lyrics');
+  while (lyricsDisplay.firstChild) {
+    lyricsDisplay.removeChild(lyricsDisplay.firstChild);
+  }
+  const currentLine = store.getState().songLyricsArray[store.getState().arrayPosition];
+  const renderedLine = document.createTextNode(currentLine);
+  document.getElementById('lyrics').appendChild(renderedLine);
+}
+window.onload = function(){
+  renderLyrics();
+}
 // CLICK LISTENER
 const userClick = () => {
   console.log('click');
+  store.dispatch({ type: 'NEXT_LYRIC' } );
+  console.log(store.getState());
 }
+//SUBSCRIBE TO REDUX STORE
+store.subscribe(renderLyrics);
